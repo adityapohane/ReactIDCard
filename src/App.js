4a3e3c9a28d28a59a77100940d7e0f0962819b03
adityapohane <adityapohane9897@gmail.com>
2023-04-17 11:22:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './img/profile.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="maindiv">
+<center>
+  <h3>axiom techguru pvt lmt</h3>
+<img src={logo} alt="abc" />
+    <table >
+      <tr><td className='headings'>Full Name :</td><td>Aditya Pohane</td></tr>
+      <tr><td className='headings'>Email :</td><td>pohaneaditya123@gmail.com</td></tr>
+      <tr><td className='headings'>DOB :</td><td>15/05/2000</td></tr>
+      <tr><td className='headings'>Department :</td><td>Software Developer</td></tr>
+      <tr><td className='headings'>phone Number :</td><td>0621 235 3445</td></tr>
+    <tr><td className='headings'>Address :</td><td>Nagpur,Maharashtra,India</td></tr>
+    </table>
+
+
+</center>
     </div>
   );
 }
